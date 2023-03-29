fc21d245068605a6dc109675cae7746c9aaf444e
Valentin Wellenstein <valentin.wellenstein@etu.minesparis.psl.eu>
2023-03-29 16:44:04 +0200

    Partie Méca

--- projet1.py
+++ projet1.py
@@ -2,29 +2,31 @@
 #Importation
 import math as ma
 
-#Définition des variables
+##Définition des variables##
 M = 140000 #en kg, pour une rame
-g = 9,81 
+g = 9.81 
 alpha = [0 for e in range(500)]
 
-#Calcul de la vitesse
-#Hypothèse : profil de vitesse trapézoïdal, vitesse max de 60 km/h atteinte au 1er km. On freine au 4ème km.
+##Calcul de la vitesse##
+#Hypothèse : profil de vitesse trapézoïdal, vitesse max de 60 km/h atteinte au 100ème pas de temps. On freine au 400 pas de temps.
 V=[]
 for i in range(100):
-    V.append(0,6*i)
+    V.append(0.6/3.6*i)
 for i in range(300):
-    V.append(60)
+    V.append(60/3.6)
+for i in range(100):
+    V.append(60/3.6-0.6/3.6*i)
+
+A=[]
+for i in range(100):
+    A.append(0.6/(3.6**3.6*1000))
+for i in range(300):
+    A.append(0)
 for i in range(400,500):
-    V.append(60-0,6*i)
+    A.append(-0.6/(3.6**3.6*1000))
 
-#Calcul Puissance Train
-def Puissance_Train ():
-    P = []
-    for i in range(500):
-        P.append((M*g*ma.sin(alpha[i])+resistance_mouvement[i])*V[i])
-    return(P)
-
-def resistance_mouvement(V):
+##Calcul Puissance Train##
+def frottements():
     a=0.3
     b=0.1
     l=[]
@@ -32,11 +34,14 @@ def resistance_mouvement(V):
         l.append(1000+ a*V[i] + b*V[i]**2)
     return l
 
-def force_freinage(a,V): #vitesse definie
-    l=[0 for i in range(V)]
-    for i in range(400,len(V)):
-        l[i]=M*a+resistance_mouvement(V[i])
-    return l
 
+def Puissance_Train():
+    P = []
+    for i in range(1,400):
+        P.append((M*A[i] + M*g*ma.sin(alpha[i]) + frottements()[i]) * V[i])
+    for i in range(100):
+        P.append((M*A[i] + M*g*ma.sin(alpha[i]) - frottements()[i]) * V[i])
+    return(P)
 
-#Calcul Puissance Électrique
+#print(frottement_air())
+##Calcul Puissance Électrique##
