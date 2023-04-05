d5fc162da561a237fdea823043155692e6663863
Valentin Wellenstein <valentin.wellenstein@etu.minesparis.psl.eu>
2023-04-05 14:28:10 +0200

    Ajout Courbe

--- projet1.py
+++ projet1.py
@@ -1,12 +1,13 @@
 #coucou 
 #Importation
 import math as ma
+import matplotlib.pyplot as plt
 
 ##Définition des variables##
 M = 140000 #en kg, pour une rame
 g = 9.81 
 alpha = [0 for e in range(500)]
-
+tau = 1 #pas de temps
 ##Calcul de la vitesse##
 #Hypothèse : profil de vitesse trapézoïdal, vitesse max de 60 km/h atteinte au 100ème pas de temps. On freine au 400 pas de temps.
 V=[]
@@ -16,6 +17,13 @@ for i in range(300):
     V.append(60/3.6)
 for i in range(100):
     V.append(60/3.6-0.6/3.6*i)
+
+X=[]
+position = 0
+for i in range(500):
+    position = position + V[i]*tau
+    X.append(position)
+
 
 A=[]
 for i in range(100):
@@ -47,16 +55,24 @@ def Puissance_Train():
 ##Calcul Puissance Électrique##
 
 #Définition des variables pour le circuit électrique
-Is1 = #courant provenant du cable venant de la sous-station (SS) 1
-Is2 = #courant provenant du cable venant de la SS2
-Rs1 = #résistance interne des SS
+Is1 = 0 #courant provenant du cable venant de la sous-station (SS) 1
+Is2 = 0 #courant provenant du cable venant de la SS2
+Rs1 = 0 #résistance interne des SS
 Rs2 = Rs1
 Rl = 0.016 * 10**(-3)#résistance linéique cable entre SS1 et train (en Ohm par m)
 Ud0 = 835 #en V
 L = 5000 #en m, distance entre chaque sous-station
+U_train = []
+I_train = []
 
 #Calcul Puissance Électrique
 def Puissance_Electrique (Is1, Is2, position):
-    U_train = Ud0-Is2/(Rs2 + Rl * position)
-    I_train = Is1 + Is2
-    return U_train * I_train
+    U_train.append(float(Ud0-Is2/(Rs2 + Rl * X[i])))
+    I_train.append(float(Is1 + Is2))
+    return (U_train * I_train)
+
+##Calcul des courbes
+plt.figure()
+plt.plot(U_train,X)
+plt.title("Tension en fonction de la position")
+plt.show()
