b3b48886b5861b075b7b46f7f46bfd6b6d5fd25b
Valentin Wellenstein <valentin.wellenstein@etu.minesparis.psl.eu>
2023-04-05 16:27:24 +0200

    Ajout Puissance recuperee

--- projet1.py
+++ projet1.py
@@ -1,13 +1,17 @@
 # coucou
+
 # Importation
 import math as ma
 import matplotlib.pyplot as plt
+
 
 ##Définition des variables##
 M = 140000  # en kg, pour une rame
 g = 9.81
 alpha = [0 for e in range(500)]
 tau = 1  # pas de temps
+
+
 ##Calcul de la vitesse##
 # Hypothèse : profil de vitesse trapézoïdal, vitesse max de 60 km/h atteinte au 100ème pas de temps. On freine au 400 pas de temps.
 V = []
@@ -23,7 +27,6 @@ position = 0
 for i in range(500):
     position = position + V[i] * tau
     X.append(position)
-
 
 A = []
 for i in range(25):
@@ -42,17 +45,17 @@ def frottements():
         l.append(1000 + a * V[i] + b * V[i] ** 2)
     return l
 
-
 def Puissance_Train():
     P = []
     for i in range(400):
         P.append((M * A[i] + M * g * ma.sin(alpha[i]) + frottements()[i]) * V[i])
-    for i in range(100):
-        P.append(0)
+    for i in range(400,499): #Calcul Puissance récupérée par freinage
+        P.append(-0.20*(0,5*M*(V[i]**2-V[i+1]**2))) #On récupère 20% de l'énergie cinétique, et on prends l'accélération et la vitesse au point juste avant de freiner
     return P
 
-
 P = Puissance_Train()
+print(P)
+
 
 ##Calcul Puissance Électrique##
 
@@ -60,7 +63,6 @@ P = Puissance_Train()
 Is1 = 1200  # courant provenant du cable venant de la sous-station (SS) 1
 Is2 = 1200  # courant provenant du cable venant de la SS2
 Rs1 = 0.1  # résistance interne des SS négligeable
-
 Rs2 = Rs1
 Rl = 0.016 * 10 ** (-3)  # résistance linéique cable entre SS1 et train (en Ohm par m)
 Ud0 = 835  # en V
@@ -70,7 +72,6 @@ I_train = []
 W_train = []
 
 # Calcul Puissance Électrique
-
 for i in range(500):
     U_train.append(Ud0 - Is2 * (Rs2 + Rl *(X[500]- X[i]))
     I_train.append(Is1 + Is2)
